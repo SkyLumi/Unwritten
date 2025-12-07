b19b5a68c931b45d00f78404ee9fbdea19b82b0e
fabiannabil1 <142811627+fabiannabil1@users.noreply.github.com>
2025-12-07 17:55:28 +0700

    .

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -8,7 +8,10 @@ extends CharacterBody3D
 @onready var collider = $Collider
 
 
+@onready var respawn_point : Vector3
+
 func _ready():
+	respawn_point = global_position
 	visuals.accept_model(model)
 	#$CameraMount/PlayerCamera.current = false
 	#print_tree_pretty()
@@ -27,6 +30,23 @@ func _physics_process(delta):
 	if Engine.get_physics_frames() % 60 == 0:
 		print("AFTER model.update, BEFORE move_and_slide - velocity: ", velocity)
 	
+	# Fall detection
+	if global_position.y < -2:
+		die_and_respawn()
+		
 	move_and_slide()
+
+# New function for respawn logic
+func die_and_respawn():
+	global_position = respawn_point
+	velocity = Vector3.ZERO
+	model.reset_jump_count() # Reset jump count just in case
+	
+	# Reset resources
+	if model.resources:
+		model.resources.health = model.resources.max_health
+		model.resources.stamina = model.resources.max_stamina
+
+
 	
 	# Visuals -> follow parent transformations
